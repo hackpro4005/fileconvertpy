d8bf1382e33c1ef58dfd821db1c50384586ef214
hackpro4005 <114504302+hackpro4005@users.noreply.github.com>
2024-04-19 19:29:24 +0300

    commited setup

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
-import os
+from setuptools import setup
 
-def create_distribution():
-    source_dir = 'fileconvertpy'
-    dist_dir = 'dist'
-    os.makedirs(dist_dir, exist_ok=True)
-    for filename in os.listdir(source_dir):
-        if filename.endswith('.py') or filename.lower() == 'readme.md':  # Include Python files and README
-            with open(os.path.join(source_dir, filename), 'r') as source_file:
-                with open(os.path.join(dist_dir, filename), 'w') as dest_file:
-                    dest_file.write(source_file.read())
-
-if __name__ == "__main__":
-    create_distribution()
+setup(
+    name='fileconvertpy',
+    version='0.1',
+    packages=['fileconvert'],
+    install_requires=[],
+)
